8f0d6d08c1a2f3a444418acd2418dae2f4762734
Sh1zavibe <danilchukmatvey871@gmail.com>
2026-05-25 10:52:21 +0300

    all

--- IGI/LR5/ZooProject/animals/middleware.py
+++ IGI/LR5/ZooProject/animals/middleware.py
@@ -1,18 +1,13 @@
-# animals/middleware.py
-
 from django.utils import timezone
-from django.conf import settings
 
 class TimezoneMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Получаем часовой пояс из сессии (если есть)
         tzname = request.session.get('django_timezone')
-        if tzname and tzname in settings.SUPPORTED_TIMEZONES:
+        if tzname:
             timezone.activate(tzname)
         else:
-            # Если не задан – деактивируем, будет использоваться UTC
             timezone.deactivate()
         return self.get_response(request)
